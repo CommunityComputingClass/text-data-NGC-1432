d6dcd039683abaabbca8db8120fa0493e0dd25e3
NGC-1432 <131916088+NGC-1432@users.noreply.github.com>
2025-01-28 23:23:00 +0000

    not quite brat green, but close enough

--- project/mySketch.js
+++ project/mySketch.js
@@ -1,8 +1,13 @@
+let theMessage = "You will survive"
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
-  background(200);
+  background("lime");
+  noStroke();
+  fill("black");
+  text(theMessage, 175, 200)
   
 }
