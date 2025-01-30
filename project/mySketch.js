bd3bfb763a0c380a75c150a64df196f2a86993b5
NGC-1432 <131916088+NGC-1432@users.noreply.github.com>
2025-01-30 21:13:39 +0000

    font files uploaded

--- project/mySketch.js
+++ project/mySketch.js
@@ -1,4 +1,9 @@
 let theMessage = "You will survive"
+let myFont;
+
+function preload(){
+  myFont = loadFont("CloisterBlack.ttf")
+}
 
 function setup() {
   createCanvas(400, 400);
@@ -8,6 +13,7 @@ function draw() {
   background("lime");
   noStroke();
   fill("black");
+  textFont(myFont)
   text(theMessage, 175, 200)
   
 }
