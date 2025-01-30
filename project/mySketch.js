4cd0b5b9c83897d95d01028ebe207df4d2825288
NGC-1432 <131916088+NGC-1432@users.noreply.github.com>
2025-01-30 22:05:28 +0000

    fonts added and now working on dynamic code that makes things happen when you press buttons

--- project/mySketch.js
+++ project/mySketch.js
@@ -1,4 +1,5 @@
-let theMessage = "You will survive"
+let headline = "Generic Placeholder Headline"
+let surveyquestion = "Press any button if you like ice cream"
 let myFont;
 
 function preload(){
@@ -10,10 +11,22 @@ function setup() {
 }
 
 function draw() {
-  background("lime");
-  noStroke();
+  background("white");
   fill("black");
+  textSize(25)
   textFont(myFont)
-  text(theMessage, 175, 200)
-  
+  text(headline, 25, 200)
+  SurveyQuery(25,250)
+}
+
+function SurveyQuery(x,y){
+  fill("black")
+  textSize(20)
+  textFont(myFont)
+  text(surveyquestion, x, y)
+}
+
+function keyPressed(){
+  fill("green")
+  ellipse(random(0,400),random(0,400),10,10)
 }
