bb92607061e55b26b57e210ed044704f13a28691
NGC-1432 <131916088+NGC-1432@users.noreply.github.com>
2025-02-10 23:26:00 +0000

    working on dynamic text entry

--- project/mySketch.js
+++ project/mySketch.js
@@ -1,7 +1,7 @@
-let headline = "Generic Survey Title"
-let surveyquestion = "Press the left arrow if thee would have thy ice cream in a cone, or the right arrow if thou wouldst have it from a cup."
+let headline = "Query of Great Import"
+let surveyquestion = "What is thy favorite song?"
 let myFont;
-let balance = 0
+let contents = "";
 
 function preload(){
   myFont = loadFont("CloisterBlack.ttf")
@@ -21,15 +21,7 @@ function draw() {
   fill("black");
   textSize(20)
   textFont(myFont)
-  text(balance,40,170)
-  if (balance < 0){
-    fill("azure")
-    rect([400+balance],150,[0-balance],30)
-  }
-  if (0 < balance){
-    fill("lavender")
-    rect(400,150,[0+balance],30)
-  }
+  text(contents, 25,150,200,600)
 }
 
 function SurveyQuery(x,y){
@@ -39,11 +31,6 @@ function SurveyQuery(x,y){
   text(surveyquestion, x, y)
 }
 
-function keyPressed(){
-  if(keyCode === LEFT_ARROW){
-    balance = balance -1;
-  }
-  if(keyCode === RIGHT_ARROW){
-    balance = balance +1;
-  }
+function keyTyped() {
+  contents = contents + key;
 }
---
+++ project/surveyCode.js
@@ -0,0 +1,50 @@
+let headline = "Generic Survey Title"
+let surveyquestion = "Press the left arrow if thee would have thy ice cream in a cone, or the right arrow if thou wouldst have it from a cup."
+let myFont;
+let contents = "";
+let balance = 0
+
+function preload(){
+  myFont = loadFont("CloisterBlack.ttf")
+}
+
+function setup() {
+  createCanvas(1000, 800);
+}
+
+function draw() {
+  background("white");
+  fill("black");
+  textSize(25)
+  textFont(myFont)
+  text(headline, 25, 75)
+  SurveyQuery(25,100)
+  fill("black");
+  textSize(20)
+  textFont(myFont)
+  text(balance,40,170)
+  if (balance < 0){
+    fill("azure")
+    rect([400+balance],150,[0-balance],30)
+  }
+  if (0 < balance){
+    fill("lavender")
+    rect(400,150,[0+balance],30)
+  }
+}
+
+function SurveyQuery(x,y){
+  fill("black")
+  textSize(20)
+  textFont(myFont)
+  text(surveyquestion, x, y)
+}
+
+function keyPressed(){
+  if(keyCode === LEFT_ARROW){
+    balance = balance -5;
+  }
+  if(keyCode === RIGHT_ARROW){
+    balance = balance +5;
+  }
+}
